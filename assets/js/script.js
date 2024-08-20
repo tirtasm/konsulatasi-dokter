16f58057b5abdb50e5f073b55125755a1ee357ba
tirtasm <tirtasamudraa330@gmail.com>
2024-08-20 11:20:05 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+  $(document).ready(function(){
+  $('.slick-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $('.slick-prev'),
+    nextArrow: $('.slick-next'),
+  });
+});
